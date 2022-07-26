9d6fdc69ab3896e3563b08b414c18ef3765d8278
ValeriiaM23 <mezencevavalerie@gmail.com>
2022-07-26 19:00:35 +0200

    fixing js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -11,12 +11,3 @@ const ul = document.querySelector('#ingredients')
 const itemsEl = ingredients.map(name => `<li class='item'>${name}</li>`)
 ul.insertAdjacentHTML('afterbegin', itemsEl.join(''))
 console.log(ul);
-// ingredients.forEach(name => {
-//   const item = document.createElement('li');
-//   item.textContent = name;
-//   item.classList = 'item'
-//   console.log(item);
- 
-// });
-
-//  document.ul.appendChild(ingredients);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,6 +1,6 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
 const spanColor = document.querySelector('.color');
 const buttonColor = document.querySelector('.change-color');
 const body = document.querySelector('body');
@@ -8,6 +8,6 @@ const body = document.querySelector('body');
 function changeColor() {
   body.style.backgroundColor = getRandomHexColor();
   spanColor.textContent = getRandomHexColor();
-
 }
-buttonColor.addEventListener('click', changeColor);
+
+buttonColor.addEventListener('click', changeColor); 
